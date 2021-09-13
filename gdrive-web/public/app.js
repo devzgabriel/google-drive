0f08772df4d7a8eeca4a7caf945005fb19e7cadc
Gabriel Silva <gabriel.silva@eurekalabs.com.br>
2021-09-12 21:32:20 -0300

    Web

--- gdrive-web/public/app.js
+++ gdrive-web/public/app.js
@@ -1,1 +1,20 @@
-console.debug('hello world!')
+import AppController from './src/appController.js'
+import ConnectionManager from './src/connectionManager.js'
+import DragAndDropManager from './src/dragAndDropManager.js'
+import ViewManager from './src/viewManager.js'
+
+const API_URL = 'https://0.0.0.0:3000'
+
+const appController = new AppController({
+  viewManager: new ViewManager(),
+  dragAndDropManager: new DragAndDropManager(),
+  connectionManager: new ConnectionManager({
+    apiUrl: API_URL,
+  }),
+})
+
+try {
+  await appController.initialize()
+} catch (error) {
+  console.error('error on initializing', error)
+}
